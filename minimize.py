23bd2a8c263ac895a70068fc850f901f16975055
Josh Pfosi <joshpfosi@gmail.com>
2015-03-13 01:35:42 -0400

    Basic testing architecture

--- minimize.py
+++ minimize.py
@@ -7,18 +7,31 @@ __author__ = "Josh Pfosi"
 #       dcs  - the don't cares of the function
 # Notes: All minterms which are not in ones or dcs are assumed zeros
 # This interface is deliberately similar to the qm library for testing purposes
-def minimize():
-  return ['X01', '0X0']
+def minimize(ones=None, dc=None):
+  return qm.qm(ones=ones, dc=dc)
 
 if __name__ == "__main__":
   import qm
+  import sys
+  from numpy.random import randint
+  from numpy import unique
 
-  lib_res = qm.qm(ones=[1, 2, 5], dc=[0, 7])
-  my_res  = minimize()
-  
-  if lib_res == my_res:
-    print "."
-  else:
-    print "F"
+  ones = [0,1,4,7,4]
+  dc   = []
+
+  for i in range(0,3):
+    # calculate both
+    lib_res = qm.qm(ones=ones, dc=dc)
+    my_res  = minimize(ones=ones, dc=dc)
+    
+    # ensure order doesn't matter
+    lib_res.sort()
+    my_res.sort()
+
+    # test and print
+    if lib_res == my_res:
+      sys.stdout.write(".")
+    else:
+      sys.stdout.write("F")
 
 
